ee82c8cf674f2ec3b746198e46f8a65578b0c104
solo-mizan <mizanbd.eee@gmail.com>
2022-04-26 00:25:12 +0600

    delete method added

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -10,14 +10,31 @@ const Home = () => {
     }, []);
 
     const handleUserDelete = (id) => {
-        const url = `http://localhost:5000/user/:${id}`
+        console.log('deleting user with id, ', id);
+        const url = `http://localhost:5000/user/${id}`;
         fetch(url, {
             method: 'DELETE'
         })
             .then(res => res.json())
-            .then(data => console.log(data))
-        console.log('deleting', id);
+            .then(data => {
+                if (data.deletedCount > 0) {
+                    console.log('deleted');
+                    const remaining = users.filter(user => user._id !== id);
+                    setUsers(remaining);
+                }
+            })
+
+
     }
+
+    //     const url = `http://localhost:5000/user/${id}`
+    //     fetch(url, {
+    //         method: 'DELETE'
+    //     })
+    //         .then(res => res.json())
+    //         .then(data => console.log(data))
+    //     console.log('deleting', id);
+    // }
 
     return (
         <div>
