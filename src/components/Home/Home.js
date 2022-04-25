847bd22f8c225126e7c02333899e9a6c2880e07d
solo-mizan <mizanbd.eee@gmail.com>
2022-04-26 01:54:10 +0600

    update page added

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from 'react';
+import { Link } from 'react-router-dom';
 
 const Home = () => {
     const [users, setUsers] = useState([]);
@@ -45,6 +46,7 @@ const Home = () => {
                 {
                     users.map(user => <li key={user._id}>
                         {user.name}
+                        <Link to={`/user/update/:${user._id}`}><button>Update</button></Link>
                         <button onClick={() => handleUserDelete(user._id)}>X</button>
                     </li>)
                 }
